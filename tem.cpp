4752711e1e657f6998a3618a0730eb0c574ba1aa
Ther-hh <1348927236@qq.com>
2021-09-24 16:19:32 +0800

    更改tem.cpp

--- tem.cpp
+++ tem.cpp
@@ -2,11 +2,10 @@
 int main()
 {
 	int x,y;
-	double m,n;
 	x = 2;
 	y = 1;
-	scanf("%d %d %f %f",&x,&y,&m,&n);
-	printf("%d %d %d %d\n",x,y,m,n);
+	scanf("%d %d",&x,&y);
+	printf("%d %d\n",x,y);
 	printf("hello world!\n");
 	printf("%d",x + y);
 }
