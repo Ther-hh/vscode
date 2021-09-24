2cfa0a22d1c7c802fe899e4a39e5f1a5b97b06fc
Ther-hh <1348927236@qq.com>
2021-09-24 16:14:15 +0800

    加入tem.cpp

---
+++ tem.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+int main()
+{
+	int x,y;
+	double m,n;
+	x = 2;
+	y = 1;
+	scanf("%d %d %f %f",&x,&y,&m,&n);
+	printf("%d %d %d %d\n",x,y,m,n);
+	printf("hello world!\n");
+	printf("%d",x + y);
+}
