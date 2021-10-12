5d1c0fd5ebfb9ee4c2b3676eb4857d076e07c17f
Ther-hh <1348927236@qq.com>
2021-10-12 14:21:29 +0800

    更改tem

--- tem.cpp
+++ tem.cpp
@@ -1,11 +1,36 @@
-#include<stdio.h> 
+/*#include<stdio.h> 
 int main()
 {
 	int x,y;
-	x = 2;
-	y = 1;
-	scanf("%d %d",&x,&y);
+	char a;
+	scanf("%d %d %c",&x,&y,&a);//shuru
 	printf("%d %d\n",x,y);
-	printf("hello world!\n");
-	printf("%d",x + y);
+	//printf("hello world!\n");
+	printf("%d%c",x + y,a);
+}*/
+
+#include <stdio.h>
+int main()
+{
+	int a,b,c;
+	scanf("%d %d %d",&a,&b,&c);
+	if(a+b<=c && a+c<=b && b+c<=a){
+		printf("non-triangle.\n");
+	}
+	else if(a == b||b == c){
+		printf("equilateral triangle.\n");
+	}
+	else if(a == b){
+	    printf("isoceles triangle.\n");
+	}
+	else if(b == c){
+	    printf("isoceles triangle.\n");
+	}
+	else if(a == c){
+	    printf("isoceles triangle.\n");
+	}
+	else{
+	printf("triangle.\n"); 
+	}
+	//system("pause");
 }
